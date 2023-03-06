aa2e424bdb6ccc71150b26e52632dc8d93a8139f
Log-Wall <mrwallacelk@gmail.com>
2023-03-05 21:34:31 -0800

    tags

--- src/base/nexaction.js
+++ src/base/nexaction.js
@@ -5,7 +5,7 @@ const CreateTrigger = ({
   regex,
   action,
   id,
-  group,
+  tags,
   enabled,
   once,
   duration,
@@ -29,7 +29,7 @@ const CreateTrigger = ({
     regex: regex,
     action: action,
     id: reflexId,
-    group: group,
+    tags: tags,
     enabled: enabled,
     once: once,
     timer: timer,
@@ -43,13 +43,19 @@ const CreateHandler = () => {
     regex,
     action,
     id = crypto.randomUUID(),
-    group = false,
+    tags = [],
     enabled = true,
     once = false,
     duration = false,
   }) => {
+    if (reflexes.find((e) => e.id === id)) {
+      console.log(
+        `[nexAction] Attempted to create reflex with non-unique ID ${id} reassigning to UUID`
+      );
+      id = crypto.randomUUID();
+    }
     reflexes.push(
-      CreateTrigger({ regex, action, id, group, enabled, once, duration })
+      CreateTrigger({ regex, action, id, tags, enabled, once, duration })
     );
   };
 
@@ -85,7 +91,7 @@ const CreateHandler = () => {
         try {
           reflex.action(args);
         } catch (error) {
-          console.log(reflex?.group);
+          console.log(reflex?.tags);
           console.log(reflex.regex);
           console.log(error);
         }
@@ -104,18 +110,24 @@ const CreateHandler = () => {
     reflexes.length = 0;
   };
 
-  const enable = (id, group = true) => {
-    if (group) {
-      reflexes.filter((e) => e.group === id).forEach((e) => (e.enabled = true));
+  const enable = (identifier) => {
+    if (Array.isArray(identifier)) {
+      reflexes
+        .filter((reflex) =>
+          identifier.every((tag) => reflex.tags.indexOf(tag) > -1)
+        )
+        .forEach((e) => (e.enabled = true));
     } else {
       reflexes.filter((e) => e.id === id).forEach((e) => (e.enabled = true));
     }
   };
 
-  const disable = (id, group = true) => {
-    if (group) {
+  const disable = (identifier) => {
+    if (Array.isArray(identifier)) {
       reflexes
-        .filter((e) => e.group === id)
+        .filter((reflex) =>
+          identifier.every((tag) => reflex.tags.indexOf(tag) > -1)
+        )
         .forEach((e) => (e.enabled = false));
     } else {
       reflexes.filter((e) => e.id === id).forEach((e) => (e.enabled = false));
